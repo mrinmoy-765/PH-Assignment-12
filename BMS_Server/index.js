2bc4627b01dda7b6077e71a174370bf1eeb5a744
Mrinmoy <mrinmoyd629@gmail.com>
2025-06-14 17:00:22 +0600

    database setup

---
+++ BMS_Server/index.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+//middleware
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: ["https://elaborate-kangaroo-16f8b1.netlify.app"],
+//     credentials: true,
+//   })
+// );
+
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncoxxcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+
+    const BMS_userCollection = client.db("BMS").collection("BmsUsers");
+
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies.accessToken;
+      //  console.log("indise verify token", token);
+
+      if (!token) return res.status(401).send("Unauthorized");
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).send("Forbidden");
+        req.user = decoded;
+        next();
+      });
+    };
+
+    //Auth related APIs
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // Create JWT token
+      const accessToken = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      // Set cookies
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      res.send({ sucess: true });
+    });
+
+    //clear cookie
+    app.post("/logout", (req, res) => {
+      res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      return res.status(200).json({ message: "Logged out successfully" });
+    });
+
+    //create User
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      // console.log(newUser);
+      const result = await BMS_userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    //  await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("BMS server is running");
+});
+
+app.listen(port, () => {
+  console.log(`BMS is running on port: ${port}`);
+});
